22493e8efd3a5ff7b4eb20d43029728af879ae06
DarKS4ys <melihyardim1057@gmail.com>
2024-02-25 23:32:02 +0300

    test vercel edgestore

--- app/api/whisper/route.ts
+++ app/api/whisper/route.ts
@@ -1,15 +1,18 @@
 // Import necessary libraries
+import { useEdgeStore } from '@/lib/storage';
+import { backendClient } from '@/lib/storage-server';
 import { exec } from 'child_process';
 import fs from 'fs';
 import { NextRequest, NextResponse } from 'next/server';
 import os from 'os';
+import { Readable } from 'stream';
 
 const util = require('util');
 const execAsync = util.promisify(exec);
-
 const OpenAI = require('openai');
 const openai = new OpenAI(process.env.OPENAI_API_KEY);
 export async function POST(request: NextRequest) {
+
   const req = await request.json();
   // Extract the audio data from the request body
   const base64Audio = req.audio;
@@ -34,7 +37,45 @@ export async function POST(request: NextRequest) {
     }
   }
 }
+/*  console.log(os.tmpdir()) */
+
+async function convertAudioToMp3(audioData: any) {
+  try {
+    // Write the audio data to a file
+    const webmFile = await backendClient.publicFiles.upload({
+      content: {
+        blob: new Blob([audioData], { type: 'audio/webm' }),
+        extension: 'webm',
+      },
+      options: {
+        temporary: true,
+      },
+      ctx: {
+        userId: '123',
+        userRole: 'admin',
+      },
+      input: {
+        type: 'post',
+      },
+    });
+
+    // Convert the audio to MP3 using ffmpeg
+    const outputPath = `tmp/output.mp3`;
+    await execAsync(`ffmpeg -i ${webmFile.url} ${outputPath}`);
+    // Read the converted audio data
+
+    const mp3AudioData = fs.readFileSync(outputPath);
+    
+    fs.unlinkSync(outputPath);
+
+    return mp3AudioData;
+  } catch (error) {
+    console.log(error);
+  }
+}
+
 async function convertAudioToText(audioData: any) {
+  
   try {
     const mp3AudioData = await convertAudioToMp3(audioData);
 
@@ -42,51 +83,43 @@ async function convertAudioToText(audioData: any) {
       throw new Error('Failed to convert audio to MP3.');
     }
 
-    const outputPath = `${os.tmpdir()}/output.mp3`;
-    fs.writeFileSync(outputPath, mp3AudioData);
-    
-    const response = await openai.audio.transcriptions.create({
-      file: fs.createReadStream(outputPath),
-      model: 'whisper-1',
-      language: "tr"
+    const res = await backendClient.publicFiles.upload({
+      content: {
+        blob: new Blob([mp3AudioData], { type: 'audio/mpeg' }),
+        extension: 'mp3',
+      },
+      options: {
+        temporary: true,
+      },
+      ctx: {
+        userId: '123',
+        userRole: 'admin',
+      },
+      input: {
+        type: 'post',
+      },
     });
-    fs.unlinkSync(outputPath);
-    const transcribedText = response.text
-  
-/*      console.log(transcribedText); */
-     return transcribedText;
+
+    const mp3FileUrl = res.url;
+    const fetchedResponse = await fetch(mp3FileUrl);
+    const mp3Buffer = await fetchedResponse.arrayBuffer();
+    const blob = new Blob([mp3Buffer], { type: 'audio/mpeg' });
+    const file = new File([blob], 'audio.mp3', { type: 'audio/mpeg' });
+    
+/*     const outputPath = `${os.tmpdir()}/output.mp3`;
+    fs.writeFileSync(outputPath, mp3AudioData); */
+
+    const response = await openai.audio.transcriptions.create({
+      file: file,
+      model: 'whisper-1',
+      language: 'tr',
+    });
+/* 
+    fs.unlinkSync(outputPath); */
+    const transcribedText = response.text;
+
+    return transcribedText;
   } catch (err) {
     console.error(err);
-  }
-}
-
-/* console.log(os.tmpdir()) */
-
-async function convertAudioToMp3(audioData: any) {
-  try {
-    // Write the audio data to a file
-    /* const inputPath = `${os.tmpdir()}/input.webm`;
-    fs.writeFileSync(inputPath, audioData); */
-
-    let writeStream = fs.createWriteStream(`/tmp/input.webm`, audioData);
-    writeStream.on('finish', async function () {
-      const outputPath = `/tmp/output.mp3`;
-      await execAsync(`ffmpeg -i /tmp/input.webm /tmp/output.mp3`);
-
-    })
-  
-    const outputPath = `/tmp/output.mp3`;
-/*     await execAsync(`ffmpeg -i ${inputPath} ${outputPath}`);
- */
-    // Read the converted audio data
-
-    const mp3AudioData = fs.readFileSync(outputPath);
-    // Delete the temporary files
-    fs.unlinkSync('/tmp/input.webm');
-    fs.unlinkSync(outputPath);
-
-    return mp3AudioData;
-  } catch (error) {
-    console.log(error);
   }
 }
